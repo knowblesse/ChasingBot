5293f5daf6ba03fda41689e3426320fd9f9a6c93
Ji Hoon Jeong <knowblesse@gmail.com>
2022-02-11 10:09:16 +0900

    emergency stop added

--- chasingbot/chasingbot.ino
+++ chasingbot/chasingbot.ino
@@ -182,7 +182,7 @@ void setup()
           Serial1.println("Quick Test Mode");
           pParam.cndParam.habituation_time = 10;
           pParam.cndParam.cs_duration = 10;
-          pParam.cndParam.us_onset = 2;
+          pParam.cndParam.us_onset = 5;
           pParam.cndParam.us_duration_min = 2;
           pParam.cndParam.us_duration_max = 4;
           pParam.cndParam.isi_duration_min = 10;
@@ -262,7 +262,7 @@ void loop()
   /********************************************/
   Serial1.println("Press 'a' to start the experiment");
   // TODO : reset parameters
-  while(true)
+while(true)
   {
     if (Serial1.available() > 0 && (char(Serial1.read()) == 'a'))
     {
@@ -276,8 +276,21 @@ void loop()
   /*          Main Experiment Loop            */
   /********************************************/
 
+  bool emergency_stop = false;
+
   Serial1.println("Hab Start");
-  delay(param.habituation_time*1000);
+  long hab_onset_time_ms = millis();
+  while((millis() - hab_onset_time_ms) < param.habituation_time*1000)
+  {
+    // emergency stop
+    if (Serial1.available() > 0 && (char(Serial1.read()) == 's'))
+    {
+      Serial1.println("Emergency Stop");
+      BT.write(letter_EXEnd);
+      emergency_stop = true;
+      break;
+    }
+  }
   Serial1.println("Hab End");
 
   long trial_onset_time_ms;
@@ -290,69 +303,104 @@ void loop()
   bool isUSArmed; // if true, US is present in this trial, but not yet executed
   bool isUSOn = false;
 
-  for(int curr_trial=1; curr_trial<= param.num_trial; curr_trial++)
+  if(!emergency_stop)
   {
-    Serial1.println("+----------------------------------------------+");
-    Serial1.print("Trial : ");
-    Serial1.println(curr_trial);
-    
-    // Setup Trial Variables
-    us_duration_ms = random(param.us_duration_min*1000, param.us_duration_max*1000);
-    iti_duration_ms = random(param.isi_duration_min*1000, param.isi_duration_max*1000);
-
-    // if the Experiment Mode is Conditioning, US is armed
-    // if the Experiment Mode is Extinction or Retention, US is not armed
-    if (mode == CONDITIONING) isUSArmed = true;
-    else isUSArmed = false;
-
-    trial_onset_time_ms = millis();
-
-    // if cs_duration is more than zero, turn on the sound
-    // if cs_duration is zero, then skip the CS presentation
-    if(param.cs_duration > 0)
+    for(int curr_trial=1; curr_trial<= param.num_trial; curr_trial++)
     {
-      tmrpcm.play(pParam.musicFileName.c_str());
-      BT.write(letter_cson);
-      isCSOn = true;
-    }
+      Serial1.println("+-------------------------------------------------+");
+      Serial1.print("Trial : ");
+      Serial1.print(curr_trial);
+      
+      // Setup Trial Variables
+      us_duration_ms = random(param.us_duration_min*1000, param.us_duration_max*1000);
+      iti_duration_ms = random(param.isi_duration_min*1000, param.isi_duration_max*1000);
 
-    while(true)
-    {
-      time_from_trial_onset_ms = millis() - trial_onset_time_ms;
-
-      // check if cs_duration has reached
-      if(isCSOn && (time_from_trial_onset_ms > param.cs_duration*1000))
+      // if cs_duration is more than zero, turn on the sound
+      // if cs_duration is zero, then skip the CS presentation
+      if(param.cs_duration > 0)
       {
-        tmrpcm.disable();
-        BT.write(letter_csoff);
-        isCSOn = false;
+        tmrpcm.play(pParam.musicFileName.c_str());
+        BT.write(letter_cson);
+        Serial1.print(" CS ");
+        Serial1.print(param.cs_duration);
+        Serial1.print("s ");
+        isCSOn = true;
       }
 
-      // if US is armed, check if us_onset has reached
-      if(isUSArmed && (time_from_trial_onset_ms >= param.us_onset*1000))
+      // if the Experiment Mode is Conditioning, US is armed
+      // if the Experiment Mode is Extinction or Retention, US is not armed
+      if (mode == CONDITIONING)
       {
-        if(random(0,100) < 50) motorForward();
-        else motorBackward();
+        isUSArmed = true;
+        Serial1.print("US ");
+        Serial1.print(param.us_onset);
+        Serial1.print("-");
+        Serial1.print(int(us_duration_ms/1000));
+        Serial1.println("s");
+      else
+      {
+        Serial1.println("US X");
         isUSArmed = false;
-        isUSOn = true;
       }
 
-      // if US is on, check if us_duration has reached
-      if(isUSOn && (time_from_trial_onset_ms > (param.us_onset*1000 + us_duration_ms)))
+      trial_onset_time_ms = millis();
+
+      while(true)
       {
-        motorStop();
-        isUSOn = false;
+        time_from_trial_onset_ms = millis() - trial_onset_time_ms;
+
+        // check if cs_duration has reached
+        if(isCSOn && (time_from_trial_onset_ms > param.cs_duration*1000))
+        {
+          tmrpcm.disable();
+          BT.write(letter_csoff);
+          isCSOn = false;
+        }
+
+        // if US is armed, check if us_onset has reached
+        if(isUSArmed && (time_from_trial_onset_ms >= param.us_onset*1000))
+        {
+          if(random(0,100) < 50) motorForward();
+          else motorBackward();
+          isUSArmed = false;
+          isUSOn = true;
+        }
+
+        // if US is on, check if us_duration has reached
+        if(isUSOn && (time_from_trial_onset_ms > (param.us_onset*1000 + us_duration_ms)))
+        {
+          motorStop();
+          isUSOn = false;
+        }
+
+        // if everything is finished during this trial, exit the while loop
+        if(!isCSOn && !isUSArmed && !isUSOn) break;
+
+        // emergency stop
+        if (Serial1.available() > 0 && (char(Serial1.read()) == 's'))
+        {
+          Serial1.println("Emergency Stop");
+          BT.write(letter_EXEnd);
+          break;
+        }
       }
 
-      // if everything is finished during this trial, exit the while loop
-      if(!isCSOn && !isUSArmed && !isUSOn) break;
+      Serial1.print("ITI start. Current ITI : ");
+      Serial1.print(iti_duration_ms);
+      Serial1.println(" ms");
+      long iti_onset_time_ms = millis();
+      while((millis() - iti_onset_time_ms) < iti_duration_ms)
+      {
+        // emergency stop
+        if (Serial1.available() > 0 && (char(Serial1.read()) == 's'))
+        {
+          Serial1.println("Emergency Stop");
+          BT.write(letter_EXEnd);
+          break;
+        }
+      }
+      Serial1.println("ITI end");
     }
-
-    Serial1.print("ITI start. Current ITI : ");
-    Serial1.print(iti_duration_ms);
-    Serial1.println(" ms");
-    delay(iti_duration_ms);
-    Serial1.println("ITI end");
   }
   Serial1.println("Experiment Done");
   BT.write(letter_EXEnd);
