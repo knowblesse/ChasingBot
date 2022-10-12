b8e1151ad145c145a9cae76b30703f5d38548bb6
Knowblesse <knowblesse@gmail.com>
2022-10-12 19:33:22 +0900

    extra preprocessing part added

--- Fb/loadFibData.m
+++ Fb/loadFibData.m
@@ -30,6 +30,20 @@ data = TDTbin2mat(Path, 'VERBOSE', options.verbose);
 CSON = data.epocs.CSON.onset;
 CSOF = data.epocs.CSOF.onset;
 
+%% Parse Fiber Data
+fs = data.streams.x405C.fs;
+x465C_data = data.streams.x465C.data;
+x405C_data = data.streams.x405C.data;
+delta_data = (x465C_data - x405C_data) ./ x405C_data;
+
+%% Load Extra Commands
+Path_preprocess = fullfile(Path, 'preprocess.txt');
+if exist(Path_preprocess, 'file') > 0
+    txt = fileread(Path_preprocess);
+    eval(txt);
+end
+
+%% Check CS Data
 if size(CSON, 1) == size(CSOF, 1)
     CS = [CSON, CSOF];
     numTrial = size(CS,1);
@@ -41,12 +55,6 @@ if options.verbose
     fprintf('Total CS : %d\n', size(CS,1));
 end
 
-%% Parse Fiber Data
-fs = data.streams.x405C.fs;
-x465C_data = data.streams.x465C.data;
-x405C_data = data.streams.x405C.data;
-delta_data = (x465C_data - x405C_data) ./ x405C_data;
-
 Data = struct();
 Data.path = Path;
 Data.cs = CS;
@@ -56,9 +64,3 @@ Data.x405 = x405C_data;
 Data.delta = delta_data;
 
 end
-
-
-
-
-
-
